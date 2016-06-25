517e9bca9f6e5b83292c2e68770468f47d0859fb
Edward Zhang <edwardz123@gmail.com>
2016-06-25 14:36:06 -0700

    Added Components

---
+++ src/components/original_text.js
@@ -0,0 +1,9 @@
+import React { Component } from 'react';
+
+export default class OriginalText extends Component {
+    render() {
+        return (
+            // Write your HTML/CSS here
+        );
+    }
+}
